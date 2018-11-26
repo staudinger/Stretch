a6b81ec61dc85281bc49e07f1117dfefe4978b18
staudinger <staudinger@mail.usf.edu>
2018-11-26 07:33:01 -0500

    npm reinstall

--- stretch/src/app/app.module.ts
+++ stretch/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { GroceryListComponent } from './grocery-list/grocery-list.component';
@@ -10,7 +11,8 @@ import { GroceryListComponent } from './grocery-list/grocery-list.component';
     GroceryListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- stretch/src/app/grocery-list/grocery-list.component.ts
+++ stretch/src/app/grocery-list/grocery-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectorMatcher } from '@angular/compiler';
 
 @Component({
   selector: 'app-grocery-list',
@@ -7,15 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroceryListComponent implements OnInit 
 {
+  filter: string = 'search';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
   groceries: any[] =
   [
     {
-      "groceryName": "potato"
-    },
+      "groceryName": "potato",
+      "imageUrl": "https://openclipart.org/image/100px/svg_to_png/209652/food-potato.png"    },
     {
-      "groceryName": "tomato"
+      "groceryName": "tomato",
+      "imageUrl": "https://openclipart.org/image/100px/svg_to_png/177512/tomate.png"
     }
 ];
+  addToCart(): void 
+  {
+
+  }
 
   constructor() { }
 
