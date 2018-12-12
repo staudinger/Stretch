1ad9588c90e32299694b3099b3588f6bfbfd32a2
staudinger <staudinger@mail.usf.edu>
2018-12-12 11:13:03 -0500

    grocery-detail service and find

--- stretch/src/app/groceries/grocery-detail/grocery-detail.component.ts
+++ stretch/src/app/groceries/grocery-detail/grocery-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
 import { IGrocery } from 'src/interfaces/grocery';
+import { GroceryService } from '../grocery.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-grocery-detail',
@@ -8,15 +10,35 @@ import { IGrocery } from 'src/interfaces/grocery';
   styleUrls: ['./grocery-detail.component.css']
 })
 export class GroceryDetailComponent implements OnInit {
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  groceries: IGrocery[] = [];
+  filteredGrocery: IGrocery[] = [];
   grocery: IGrocery;
-
-  constructor(private router: Router) { }
+  name: string;
+  errorMessage: string;
+  constructor(private groceryService: GroceryService, private route: ActivatedRoute, private router: Router) {
+    this.grocery = this.groceries.filter(x=>x.groceryName == name)[0];
+    
+  }
 
   clickBack():void{
     this.router.navigate(['/groceries']);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() 
+  {
+    this.name = this.route.snapshot.paramMap.get('name');
+    this.groceryService.getGroceries().subscribe
+    (
+      //returned data (groceries) is initialized to local groceries array.
+      groceries => 
+      {
+        
+        this.groceries = groceries;
+        this.grocery = this.groceries.find(x=>x.groceryName == this.name);
+      }
 
+    );
+  }
 }
